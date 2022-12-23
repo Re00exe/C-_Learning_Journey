06af83d18115e164db2312607c6da41cee489282
re00exe <mosta02idf@gmail.com>
2022-12-23 12:09:03 +0100

    Done

---
+++ C++#Modules/C++ - Module05/ex00/main.cpp
@@ -0,0 +1,20 @@
+
+#include "Bureaucrat.hpp"
+
+int main()
+{
+	try {
+	Bureaucrat	Yaaso("Yaaso", 150);
+	
+		std::cout << Yaaso << std::endl;
+//		Yaaso.ReGrade();
+		Yaaso.DeGrade();
+	}
+	catch(const std::exception& exc) {
+		std::cerr << exc.what() << std::endl;
+	}
+
+	std::cout << "Yaaso" << std::endl;
+	
+	return (0);
+}
